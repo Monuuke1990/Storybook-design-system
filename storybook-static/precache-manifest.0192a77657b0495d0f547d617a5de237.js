f4ebf49e73bc0acef562491b3ad65f0fdddd0072
sharepoint admin <sharepointadmin@asiapac.local>
2020-03-26 17:27:09 +0530

    first commit

---
+++ storybook-static/precache-manifest.0192a77657b0495d0f547d617a5de237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8215e5311776a95d75b0d8357846103",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ba08af34f556439120c8.bundle.js"
+  },
+  {
+    "url": "runtime~main.ba08af34f556439120c8.bundle.js"
+  },
+  {
+    "revision": "e009924daee002dc1433",
+    "url": "static/css/main.50f6b1e1.chunk.css"
+  },
+  {
+    "revision": "a52ae44d8f83a400e6d4",
+    "url": "static/css/vendors~main.e6a86b1b.chunk.css"
+  },
+  {
+    "url": "vendors~main.ba08af34f556439120c8.bundle.js"
+  },
+  {
+    "url": "vendors~main.ba08af34f556439120c8.bundle.js.LICENSE.txt"
+  }
+]);
